33e008e468075ff4ff948120eb3fc02c4d89e357
Siim <46551195+siim-m@users.noreply.github.com>
2022-08-15 16:58:14 +1000

    change http code

--- HttpTrigger1/index.mjs
+++ HttpTrigger1/index.mjs
@@ -1,13 +1,89 @@
+import { DefaultAzureCredential } from '@azure/identity';
+import fetch from 'node-fetch';
+import appInsights from 'applicationinsights';
+
+appInsights.setup();
+const client = appInsights.defaultClient;
+
+const credential = new DefaultAzureCredential();
+
 export default async function (context, req) {
-  context.log('JavaScript HTTP trigger function processed a request.');
-
-  const name = req.query.name || (req.body && req.body.name);
-  const responseMessage = name
-    ? 'Hello, ' + name + '. This HTTP triggered function executed successfully.'
-    : 'This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.';
-
-  context.res = {
-    // status: 200, /* Defaults to 200 */
-    body: responseMessage,
+  const operationIdOverride = {
+    'ai.operation.id': context.traceContext.traceparent,
   };
+
+  const token = (await credential.getToken(`https://management.azure.com/`)).token;
+
+  try {
+    const customEvents = [];
+
+    const subscriptions = await (
+      await fetch('https://management.azure.com/subscriptions?api-version=2020-01-01', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+    ).json();
+
+    for (const subscription of subscriptions.value) {
+      const rbac = await (
+        await fetch(
+          `https://management.azure.com/subscriptions/${subscription.subscriptionId}/providers/Microsoft.Authorization/roleassignmentsusagemetrics?api-version=2019-08-01-preview`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        )
+      ).json();
+      const compute = await (
+        await fetch(
+          `https://management.azure.com/subscriptions/${subscription.subscriptionId}/providers/Microsoft.Compute/locations/australiaeast/usages?api-version=2022-03-01`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        )
+      ).json();
+
+      customEvents.push({
+        name: 'Number of Role Assignments',
+        subscriptionName: subscription.displayName,
+        subscriptionId: subscription.subscriptionId,
+        currentValue: rbac.roleAssignmentsCurrentCount,
+        limit: rbac.roleAssignmentsLimit,
+      });
+
+      compute.value.forEach((item) => {
+        customEvents.push({
+          name: item.name.localizedValue,
+          subscriptionName: subscription.displayName,
+          subscriptionId: subscription.subscriptionId,
+          currentValue: item.currentValue,
+          limit: item.limit,
+        });
+      });
+    }
+
+    customEvents.forEach((event) => {
+      client.trackEvent({
+        name: 'serviceLimitsUsage',
+        tagOverrides: operationIdOverride,
+        properties: event,
+      });
+    });
+
+    context.log('customEvents', customEvents);
+    context.res = {
+      status: 200,
+      body: customEvents,
+    };
+  } catch (err) {
+    context.log('err', err);
+    context.res = {
+      status: 400,
+      body: err,
+    };
+  }
 }
